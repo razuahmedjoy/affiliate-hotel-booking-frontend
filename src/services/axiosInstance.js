29ca19b0454957d9553d11d27c825c48dcfb06b3
razuahmedjoy <rjjoybzs420@gmail.com>
2025-04-06 00:39:59 +0600

    increased axios timeout

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_URL,  // Set your API base URL
-    timeout: 10000,  // Timeout after 10 seconds
+    timeout: 30000,  // Timeout after 10 seconds
     headers: {
         'Content-Type': 'application/json',
     },
